88be0c22bfaef2670a15820f6db6d498e36f7c62
whrjsgml0000 <whrjsgml4515@naver.com>
2025-10-29 22:32:43 +0900

    feat(lotto factory): Lotto 생성 기능 추가

--- src/main/java/lotto/model/factory/impl/LottoFactoryImpl.java
+++ src/main/java/lotto/model/factory/impl/LottoFactoryImpl.java
@@ -1,5 +1,7 @@
 package lotto.model.factory.impl;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 import lotto.model.domain.lotto.entity.Lotto;
 import lotto.model.factory.LottoFactory;
 
@@ -7,6 +9,7 @@ public class LottoFactoryImpl implements LottoFactory {
 
     @Override
     public Lotto create() {
-        return null;
+        List<Integer> list = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(list);
     }
 }
