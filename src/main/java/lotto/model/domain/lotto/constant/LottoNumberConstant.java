0f4a89f3733230a182b76705f4a23d53e4212e82
whrjsgml0000 <whrjsgml4515@naver.com>
2025-10-29 23:01:47 +0900

    refactor(lotto factory): 로또 기본값을 enum 클래스로 이동하여 상수로 관리

---
+++ src/main/java/lotto/model/domain/lotto/constant/LottoNumberConstant.java
@@ -0,0 +1,16 @@
+package lotto.model.domain.lotto.constant;
+
+public enum LottoNumberConstant {
+    DEFAULT_LOTTO_START_NUMBER(1),
+    DEFAULT_LOTTO_END_NUMBER(45),
+    DEFAULT_LOTTO_NUMBER_COUNT(6);
+    private final int value;
+
+    LottoNumberConstant(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+}
--- src/main/java/lotto/model/domain/lotto/factory/impl/LottoFactoryImpl.java
+++ src/main/java/lotto/model/domain/lotto/factory/impl/LottoFactoryImpl.java
@@ -1,5 +1,9 @@
 package lotto.model.domain.lotto.factory.impl;
 
+import static lotto.model.domain.lotto.constant.LottoNumberConstant.DEFAULT_LOTTO_END_NUMBER;
+import static lotto.model.domain.lotto.constant.LottoNumberConstant.DEFAULT_LOTTO_NUMBER_COUNT;
+import static lotto.model.domain.lotto.constant.LottoNumberConstant.DEFAULT_LOTTO_START_NUMBER;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import lotto.model.domain.lotto.entity.Lotto;
@@ -12,7 +16,8 @@ public class LottoFactoryImpl implements LottoFactory {
     private final int lottoNumberCount;
 
     public static LottoFactoryImpl defaultSetting() {
-        return new LottoFactoryImpl(1, 45, 6);
+        return new LottoFactoryImpl(DEFAULT_LOTTO_START_NUMBER.value(), DEFAULT_LOTTO_END_NUMBER.value(),
+                DEFAULT_LOTTO_NUMBER_COUNT.value());
     }
 
     public LottoFactoryImpl(int lottoStartNumber, int lottoEndNumber, int lottoNumberCount) {
