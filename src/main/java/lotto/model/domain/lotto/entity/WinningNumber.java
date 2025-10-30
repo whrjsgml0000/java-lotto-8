49ba2a95292d0daa0a6af92c7433001089c27faf
whrjsgml0000 <whrjsgml4515@naver.com>
2025-10-30 15:02:51 +0900

    feat(winning number): 중복된 당첨 번호 검증 기능 추가

--- src/main/java/lotto/model/domain/lotto/entity/WinningNumber.java
+++ src/main/java/lotto/model/domain/lotto/entity/WinningNumber.java
@@ -3,6 +3,7 @@ package lotto.model.domain.lotto.entity;
 import static lotto.model.domain.lotto.constant.LottoNumberConstant.DEFAULT_LOTTO_NUMBER_COUNT;
 
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumber {
 
@@ -17,11 +18,18 @@ public class WinningNumber {
 
     private void validate(List<Integer> regularNumbers, int bonusNumber) {
         validateRegularNumbersSize(regularNumbers);
+        validateRegularNumbersUnique(regularNumbers);
     }
 
     private static void validateRegularNumbersSize(List<Integer> regularNumbers) {
         if (regularNumbers.size() != DEFAULT_LOTTO_NUMBER_COUNT.value()) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 " + DEFAULT_LOTTO_NUMBER_COUNT.value() + "개만 입력 가능합니다.");
+        }
+    }
+
+    private static void validateRegularNumbersUnique(List<Integer> regularNumbers) {
+        if (Set.copyOf(regularNumbers).size() != DEFAULT_LOTTO_NUMBER_COUNT.value()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복된 숫자를 가져선 안됩니다.");
         }
     }
 }
