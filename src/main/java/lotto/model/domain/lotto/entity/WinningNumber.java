27132111bb8410bbc0d90dc65df1c484942514c2
whrjsgml0000 <whrjsgml4515@naver.com>
2025-10-30 14:57:01 +0900

    feat(winning number): 당첨 번호 정규 숫자 크기 검증 기능 추가

--- src/main/java/lotto/model/domain/lotto/entity/WinningNumber.java
+++ src/main/java/lotto/model/domain/lotto/entity/WinningNumber.java
@@ -1,5 +1,7 @@
 package lotto.model.domain.lotto.entity;
 
+import static lotto.model.domain.lotto.constant.LottoNumberConstant.DEFAULT_LOTTO_NUMBER_COUNT;
+
 import java.util.List;
 
 public class WinningNumber {
@@ -14,6 +16,12 @@ public class WinningNumber {
     }
 
     private void validate(List<Integer> regularNumbers, int bonusNumber) {
+        validateRegularNumbersSize(regularNumbers);
+    }
 
+    private static void validateRegularNumbersSize(List<Integer> regularNumbers) {
+        if (regularNumbers.size() != DEFAULT_LOTTO_NUMBER_COUNT.value()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 " + DEFAULT_LOTTO_NUMBER_COUNT.value() + "개만 입력 가능합니다.");
+        }
     }
 }
